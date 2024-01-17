f7324962688982fa757c4a3d9cbdf40a8894aa1a
Ethan Lee <ethan@useparagon.com>
2024-01-17 10:08:12 -0800

    New ClickUp integration

---
+++ src/integrations/clickup/config.ts
@@ -0,0 +1,12 @@
+import { IIntegrationConfig } from '@useparagon/core/integration';
+
+/**
+ * configuration for a clickup
+ */
+const config: IIntegrationConfig = {
+  description: "Sync tasks to ClickUp",
+  overviewText: "A long-form description for the integration's purpose.",
+  showWatermark: true,
+};
+
+export default config;
